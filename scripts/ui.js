7f5b4cc65091a758ea5683382464f60ceac06e44
Ken Zhou <mailofrealcev@gmail.com>
2019-12-01 12:31:44 -0500

    theoretical support for multiple forms

--- scripts/ui.js
+++ scripts/ui.js
@@ -5,7 +5,10 @@ class UI {
         this.dbref = dbref;
         this.el = el;
         this.activate = function () {
-
+            var TYPE = {
+                CHAT: 0,
+                NEWCHAT: 1
+            };
             this.vue = new Vue({
                 data: () => ({
                     dbref: this.dbref,
@@ -13,15 +16,7 @@ class UI {
                     contacts: [],
                     openedChats: [],
                     displayName: '',
-                    newConversationForm: {
-                        group: false,
-                        name: '',
-                        people: []
-                    },
-                    contactSearch: '',
-                    newConversationPerson: undefined,
-                    newConversationOpen: false,
-                    newConversationPhase: 0,
+
                 }),
                 methods: {
                     openChat(contact) {
@@ -32,61 +27,38 @@ class UI {
 
                     },
                     openNewConversation() {
-                        this.newConversationOpen = true;
+                        this.openedChats = [{
+                            type: TYPE.NEWCHAT,
+                            name: 'New Conversation',
+                            content: {
+                                group: false,
+                                name: '',
+                                people: [],
+                                contactSearch: '',
+                                phase: 0
+                            }
+                        }];
                         this.updateMDC();
-                    },
-                    closeNewConversation() {
-                        this.newConversationOpen = false;
-                        this.newConversationForm = {
-                            group: false,
-                            name: '',
-                            people: []
-                        };
-                        this.updateMDC();
-                    },
-                    toggleGroup() {
-                        this.newConversationForm.group = !this.newConversationForm.group;
                     },
                     nextPhase() {
                         if (this.newConversationForm.group && this.newConversationPhase == 1) {
                             if (this.newConversationForm.name == '') {
-                                setTimeout(() => {if (document.querySelector('#new-conversation-form input')) {
-                                    document.querySelector('#new-conversation-form input').focus();
-                                }}, 0);
+                                setTimeout(() => {
+                                    if (document.querySelector('#new-conversation-form input')) {
+                                        document.querySelector('#new-conversation-form input').focus();
+                                    }
+                                }, 0);
                                 return;
                             }
                         }
                         this.newConversationPhase = (this.newConversationPhase + 1) % 3;
                         if (!this.newConversationForm.group && this.newConversationPhase == 1)
                             this.newConversationPhase = 2;
-                        setTimeout(() => {if (document.querySelector('#new-conversation-form input')) {
-                            document.querySelector('#new-conversation-form input').focus();
-                        }}, 0);
-                        this.updateMDC();
-                    },
-                    addProfile() {
-                        var that = this;
-                        this.dbref.ref('users').orderByChild('email').equalTo(this.contactSearch).once('value').then(function (snap) {
-                            var result = snap.val();
-                            if (result) {
-                                for (var d in result) {
-                                    var complete = result[d];
-                                    complete.uid = d;
-                                    if (complete.uid == auth.currentUser.uid)
-                                        return;
-                                    if (that.newConversationForm.group) {
-                                        if (!that.newConversationForm.people.includes(d) && !that.newConversationForm.people.includes(auth.currentUser.uid))
-                                            that.newConversationForm.people.push(complete);
-                                        that.contactSearch = '';
-                                    } else {
-                                        that.newConversationForm.people = [complete];
-                                        that.contactSearch = '';
-                                    }
-                                }
-
+                        setTimeout(() => {
+                            if (document.querySelector('#new-conversation-form input')) {
+                                document.querySelector('#new-conversation-form input').focus();
                             }
-                        });
-
+                        }, 0);
                         this.updateMDC();
                     },
                     updateMDC() {
@@ -99,16 +71,12 @@ class UI {
                 },
                 computed: {
                     chatTitle() {
-                        if (this.openedChats.length == 0 && !this.newConversationOpen)
+                        if (this.openedChats.length == 0)
                             return 'No chats are open.';
-                        if (this.newConversationOpen && this.openedChats.length == 0)
-                            return 'New Conversation';
                         var list = [];
-                        for (var chat in this.openedChats) {
+                        for (var chat of this.openedChats) {
                             list.push(chat.name);
                         }
-                        if (this.newConversationOpen)
-                            list.push('New Conversation');
                         return list.join();
                     }
                 },
@@ -139,16 +107,77 @@ class UI {
                         var that = this;
                         setTimeout(() => this.dying = true, 0);
                         setTimeout(function () {
-                            var people = that.$parent.newConversationForm.people;
+                            var people = that.$parent.form.people;
                             var index = people.indexOf(that.profile.uid);
                             people.splice(index, 1);
                         }, 300);
                     }
                 },
                 created() {
-                    setTimeout(() => {this.$parent.updateMDC();}, 0);
-                    
+                    setTimeout(() => { this.$parent.$parent.$parent.updateMDC(); }, 0);
+
                 }
+            });
+            this.newConversationForm = Vue.component('new-conversation-form', {
+                template: '#newConversationFormTemplate',
+                props: { 'form': Object, 'window': Object },
+                methods: {
+                    close() {
+                        var index = this.$parent.$parent.openedChats.indexOf(this.window);
+                        this.$parent.$parent.openedChats.splice(index, 1);
+                    },
+                    nextPhase() {
+                        this.form.phase = Math.min(this.form.phase + 1, 1);
+                        setTimeout(() => {
+                            this.$refs.contactSearch.focus();
+                        }, 0);
+                        this.$parent.$parent.updateMDC();
+                    },
+                    previousPhase() {
+                        this.form.phase = Math.max(this.form.phase - 1, 0);
+                        this.$parent.$parent.updateMDC();
+                    },
+                    toggleGroup() {
+                        this.form.group = !this.form.group;
+                    },
+                    addProfile() {
+                        var that = this;
+                        this.$parent.$parent.dbref.ref('users').orderByChild('email').equalTo(this.form.contactSearch).once('value').then(function (snap) {
+                            var result = snap.val();
+                            if (result) {
+                                for (var d in result) {
+                                    var complete = result[d];
+                                    complete.uid = d;
+                                    if (complete.uid == auth.currentUser.uid)
+                                        return;
+                                    if (that.form.group) {
+                                        if (!that.form.people.includes(d) && !that.form.people.includes(auth.currentUser.uid))
+                                            that.form.people.push(complete);
+                                        that.form.contactSearch = '';
+                                    } else {
+                                        that.form.people = [complete];
+                                        that.form.contactSearch = '';
+                                    }
+                                }
+
+                            }
+                        });
+
+                        this.$parent.$parent.updateMDC();
+                    },
+                },
+                data: () => ({
+
+                }),
+
+            });
+            this.windowWrapper = Vue.component('window-wrapper', {
+                template: '#windowWrapperTemplate',
+                props: { 'window': Object },
+                data: () => ({
+                    TYPE: TYPE
+                })
+
             });
             this.vue.updateMDC();
         }
